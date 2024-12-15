e84d56184d2b21536478d75906cefc0d531a5a64
Oppie1 <atindall729@gmail.com>
2024-12-15 13:48:01 -0600

    Add files via upload
    8IfStatment

---
+++ 8IfStatment/IfStatment.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main()    // relational operators are < and >
+			// operational operators are == and != (does not equal0
+{
+	int a = 98;
+	int b = 76;
+
+	if (5 > 3) {
+		cout << "Bucky is AWESOME!!!!!!" << endl;
+	}
+
+	if (3 >= 3) {
+		cout << "Adam is AWESOME!!!!" << endl;
+	}
+
+	if (3 == 3) {					// != is not equal to...
+		cout << "Adam is just cool :/" << endl;
+	}
+
+	if (a > b) {
+		cout << "Wow you sure can code!" << endl;
+	}
+
+	return 0;
+}
